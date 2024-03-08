f304814f9bfda0fa40ebeca1a1a123316f1cb970
Rizqika Mulia Pratama <rizqikapratamaa@gmail.com>
2024-03-08 20:49:52 +0700

    feat: finish model

---
+++ src/run-bots.bat
@@ -0,0 +1,7 @@
+@echo off
+@REM start cmd /c "python main.py --logic Qika --email=qika@email.com --name=qika --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic Heuristik --email=heuristik@email.com --name=heuristik --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic Tesla --email=tesla@email.com --name=tesla --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic Maha --email=maha@email.com --name=maha --password=123456 --team etimo"
+@REM start cmd /c "python main.py --logic Manhattan --email=manhattan@email.com --name=manhattan --password=123456 --team etimo --host http://20.243.68.103:8082/api/ --board=6"
+start cmd /c "python main.py --logic Manhattan --email=manhattan@email.com --name=manhattan --password=123456 --team etimo"
